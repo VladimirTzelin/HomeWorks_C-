8fa684b3997ac729787262a502059559e9e3a6ba
Vladimir <vtzeli@mail.ru>
2023-01-20 16:56:28 +0400

    решение задачи 4

---
+++ HomeWorks_C#1.004/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+    5 -> 2, 4
+    8 -> 2, 4, 6, 8 */
+
+
+
+
+Console.Write("Введите число: ");
+int number= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Чётные числа в интервале от 0 до {number}");
+
+int index = 1;
+bool NoEvenNum = true;
+
+    while (index <= number)
+        {
+        if (index % 2 != 1)
+            {
+            Console.Write($"{index}, ");
+            NoEvenNum = false;
+            }
+        index = index + 1;
+        }
+
+        if (NoEvenNum)
+            {
+            Console.WriteLine("В этом интервале нет чётных положительных ычисел!");
+            }
+
+
+   
+    
+
