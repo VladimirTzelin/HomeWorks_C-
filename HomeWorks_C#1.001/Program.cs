e900875418197e1a3c29ee4ca8f8dda47308f2f4
Vladimir <vtzelin@mail.ru>
2023-01-20 17:09:12 +0400

    решение задачи 1

---
+++ HomeWorks_C#1.001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3 
+// error CS0029: Не удается неявно преобразовать тип "int" в "string".
+
+Console.Write("Введите число а > ");
+string str_a = Console.ReadLine();
+int a = Int32.Parse(str_a);
+// Console.WriteLine(a);
+       
+ Console.Write("Введите число b > ");
+string str_b = Console.ReadLine();
+int b = Int32.Parse(str_b);
+// Console.WriteLine(b);       
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+Console.WriteLine($"a = {a}; b = {b} ;  max =  {max}");
