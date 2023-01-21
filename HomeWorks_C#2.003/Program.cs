40af44ad341d29cb96e9ba9f52dcc29c13bcb42f
Vladimir <vtzelin@mail.ru>
2023-01-22 01:04:51 +0400

    задача 3

--- HomeWorks_C#2.003/Program.cs
+++ HomeWorks_C#2.003/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Задача 3:  Напишите программу, которая принимает на вход цифру, 
+               обозначающую день недели, и проверяет, является ли этот день выходным.
+    Примеры:   6 -> да
+               7 -> да
+               1 -> нет
+*/
+
+Console.Write(" Введите номер дня недели  ");
+int DayOfWeek = int.Parse(Console.ReadLine());
+
+if (DayOfWeek >= 1 && DayOfWeek <= 5)
+{
+    Console.Write(" Нет, это будний день");
+}
+else
+{
+    if (DayOfWeek >= 6 && DayOfWeek <= 7)
+    {
+        Console.Write(" Да, это суббота или воскресение");
+    }
+    else
+    {
+        Console.Write(" Такого дня в неделе нет ");
+    }
+}
+
+
+
+
