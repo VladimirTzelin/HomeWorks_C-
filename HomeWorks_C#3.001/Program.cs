aadcbc910c1355ffe89fbfb1a20187a59503cf38
Vladimir <vtzelin@mail.ru>
2023-01-22 20:49:02 +0400

    задача 3.1

--- HomeWorks_C#3.001/Program.cs
+++ HomeWorks_C#3.001/Program.cs
@@ -3,3 +3,32 @@
 Примеры:        14212 -> нет
                 12821 -> да
                 23432 -> да
+*/
+Console.Write("Введите число > ");
+string str_sequence = Console.ReadLine();
+int sequence = Int32.Parse(str_sequence);
+
+int revseq = sequence;
+if (sequence >= 0 && (Reverse(revseq) == sequence))
+    {
+        Console.Write($"Число {sequence} - палиндром");
+    }
+else
+    {
+     Console.Write($"Число {sequence} - не палиндром");
+    }
+
+// Функция реверса последовательности чисел
+
+int Reverse(int seq)
+{
+    int digit, stack = 0;
+    while (seq != 0)
+    {
+        digit = seq % 10;
+        stack = stack * 10 + digit;
+        seq = seq / 10;
+    }
+    return stack;
+}
+
