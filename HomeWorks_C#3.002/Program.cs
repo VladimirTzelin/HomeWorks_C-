79f3788430596347b49b0b4ae3a90bda3af7ddca
Vladimir <vtzelin@mail.ru>
2023-01-22 22:26:31 +0400

    Оформил вывод

--- HomeWorks_C#3.002/Program.cs
+++ HomeWorks_C#3.002/Program.cs
@@ -3,7 +3,9 @@
 Примеры:        A (3,6,8); B (2,1,-7), -> 15.84
                 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
+
 double x1, x2, y1, y2, z1, z2, distance = 0;
+
 Console.Write("Введите координату Х первой точки > ");
 x1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите координату Y первой точки > ");
@@ -26,12 +28,4 @@ Math.Pow((x1 - x2), 2) +
 Math.Pow((y1 - y2), 2) +
 Math.Pow((z1 - z2), 2)
 );
-Console.WriteLine($" Расстояние между точками - {distance:F2}");
-
-
-
-// double x = 0.5;
-// int stepen = 2;
-// Console.WriteLine("0.5^4 = " + Math.Pow(x, stepen));
-
-// double result1 = Math.Sqrt(16);
+Console.WriteLine($" Расстояние между точками А({x1},{y1},{z1}) и B({x2},{y2},{z2})  = {distance:F2}");
