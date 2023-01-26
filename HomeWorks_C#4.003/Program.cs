bbba47713c047964d944cccd0a2a0fdf5c80525b
Vladimir <vtzelin@mail.ru>
2023-01-26 22:40:46 +0400

    задача 4.3 создание и вывод на консоль массива

---
+++ HomeWorks_C#4.003/Program.cs
@@ -0,0 +1,57 @@
+﻿/* SЗадача 3:   Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Примеры:        1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+                6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Создаём массив длиной А и выводим его на консоль");
+
+int number = InputNumber("Введите число А: ");
+
+if (CheckNum(number))
+{   //Вывод массива
+    Console.WriteLine($"[{string.Join(", ", ArrayOfValues(number))}]"); 
+    // String.Join Метод Сцепляет элементы указанного массива или элементы коллекции, 
+    // помещая между ними заданный разделитель. 
+    // https://learn.microsoft.com/ru-ru/dotnet/api/system.string.join?view=net-7.0
+    Console.ReadLine();
+}
+else Console.Write($"Ошибка ввода числа {number}");
+
+
+// Преобразование видов (string) в  (int)
+int InputNumber(string message)
+{
+    System.Console.Write(message + "> ");
+    string str_num = Console.ReadLine();
+    int num = Int32.Parse(str_num);
+    return num;
+}
+
+
+// Проверка на не отрицательноcть значения числа
+bool CheckNum(int num)
+{
+    if (num < 0)
+    {
+        Console.Write("Укажите целое положительное число ! ");
+        return false;
+    }
+    else return true;
+}
+
+// Создаём массив  размерности num случайных чисел 
+int[] ArrayOfValues(int num)
+{
+    int[] array = new int[num];   //Размерность массива
+    for (int i = 0; i < num; i++) //Заполнение массива случайными значениями от 0 до 100
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+//Console.Write("Введите число, для задания массива: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int[] resalt = ArrayInit(num);
+//Console.WriteLine($"[{string.Join(", ", ArrayOfValues(num))}]"); //Выводите массив
+    //Console.ReadLine();
