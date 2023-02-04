5aef21974a2cdc0bbe639c6f15e4026aab68658a
Vladimir <vtzelin@mail.ru>
2023-02-04 16:50:50 +0400

    Задача* семинар 6

--- HomeWorks_C#6.003/Program.cs
+++ HomeWorks_C#6.003/Program.cs
@@ -4,7 +4,7 @@
 //          3, 5, 4, 2 -> 4
 //          3, 2, 4, 5
 
-int[] NumbersArray = new int[] {3, 2, 4, 5};  //{3, 5, 4, 2}; //{ 5, 5, 4, 3, 2 };
+int[] NumbersArray = new int[] { 3, 2, 4, 5 };  //{3, 5, 4, 2}; //{ 5, 5, 4, 3, 2 };
 int max1 = NumbersArray.Max();
 int max2 = NumbersArray.Min();
 for (int i = 0; i < 5; i++)
@@ -13,6 +13,5 @@ for (int i = 0; i < 5; i++)
     {
         max2 = NumbersArray[i];
     }
-    Console.WriteLine($"{i} : {max1} ; {max2};");
 }
 Console.Write($"{max1} ; {max2}");
