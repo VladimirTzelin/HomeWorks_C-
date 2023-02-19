a7320745f4a5238ff44d005b6f364bb8160470fd
Vladimir <vtzelin@mail.ru>
2023-02-19 14:47:00 +0400

    Треугольник Паскаля

---
+++ ТреугПаск2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+System.Console.WriteLine("Pascal Triangle Program");
+System.Console.Write("Введите количество строк: ");
+string input = System.Console.ReadLine();
+int n = Convert.ToInt32(input);
+
+for (int y = 0; y < n; y++)
+{
+    int c = 1;
+    for (int q = 0; q < n - y; q++)
+    {
+        System.Console.Write("    "); // Заполняем начала строк пробелами
+    }
+
+    for (int x = 0; x <= y; x++)
+    {
+        System.Console.Write("     {0:D} ", c); // расстояния между элементами
+        c = c * (y - x) / (x + 1);
+    }
+    System.Console.WriteLine();
+}
+System.Console.WriteLine();
+
+
